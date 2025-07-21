163821e15c2205a99df2b24d0d180943d0eb7200
Alistair Michael <alistair.michael@uq.edu.au>
2025-07-21 12:14:21 +1000

    comment

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -2,6 +2,7 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class MySuite extends AnyFunSuite {
   test("example test that succeeds") {
+    // fails
     assertCompiles("""
       enum X {
         case A
