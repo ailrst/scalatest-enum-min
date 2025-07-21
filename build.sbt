db3644c16d388d8b2efbda45551c4e24123c0784
Alistair Michael <alistair.michael@uq.edu.au>
2025-07-21 12:13:46 +1000

    working

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-val scala3Version = "3.3.6"
+val scala3Version = "3.3.5"
 
 lazy val root = project
   .in(file("."))
--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,6 +1,7 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class MySuite extends AnyFunSuite {
+  // passes
   test("example test that succeeds") {
     assertCompiles("""
       enum X {
